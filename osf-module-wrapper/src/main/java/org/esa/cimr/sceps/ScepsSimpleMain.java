43fa6cc4fd96580ecc1b4d7d0fb9ed3af300a1f5
Olaf Danne <olaf.danne@brockmann-consult.de>
2023-12-15 00:50:51 +0100

    added simple Main for first tests

---
+++ osf-module-wrapper/src/main/java/org/esa/cimr/sceps/ScepsSimpleMain.java
@@ -0,0 +1,23 @@
+package org.esa.cimr.sceps;
+
+/**
+ * Class containing just a main method for first simple tests.
+ *
+ */
+public class ScepsSimpleMain {
+
+    /**
+     * The main method.
+     *
+     * @param args - Program arguments
+     */
+    public static void main(String[] args){
+        if (args.length == 0) {
+            System.out.println("No arguments given");
+        } else {
+            for (String arg : args) {
+                System.out.println("arg = " + arg);
+            }
+        }
+    }
+}
