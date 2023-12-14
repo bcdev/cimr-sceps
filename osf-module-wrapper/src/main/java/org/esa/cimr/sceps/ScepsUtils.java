f6a5742bc4652c592839cb435ffd81a888886125
Olaf Danne <olaf.danne@brockmann-consult.de>
2023-12-14 23:21:47 +0100

    First commit of template code

---
+++ osf-module-wrapper/src/main/java/org/esa/cimr/sceps/ScepsUtils.java
@@ -0,0 +1,17 @@
+package org.esa.cimr.sceps;
+
+/**
+ * Utility class for CIMR SCEPS
+ *
+ * @author olafd
+ */
+public class ScepsUtils {
+
+    /**
+     * dummy
+     *
+     */
+    public static void doSomething() {
+        System.out.println();
+    }
+}
---
+++ osf-module-wrapper/src/test/java/org/esa/cimr/sceps/ScepsUtilsTest.java
@@ -0,0 +1,13 @@
+package org.esa.cimr.sceps;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScepsUtilsTest {
+
+    @Test
+    public void testDoSomething() {
+        assertTrue(true);
+    }
+}
