ee07c07da0206b71f72106b6dc30f7b99c6cc77f
Olaf Danne <olaf.danne@brockmann-consult.de>
2023-12-27 16:27:39 +0100

    continued tests for xml config file parsing

--- osf-scene-generation-module-wrapper/src/test/java/org/esa/cimr/sceps/ScepsConfigTest.java
+++ osf-scene-generation-module-wrapper/src/test/java/org/esa/cimr/sceps/ScepsConfigTest.java
@@ -1,28 +1,22 @@
 package org.esa.cimr.sceps;
 
-import org.esa.cimr.sceps.ScepsConfig;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import java.io.File;
-import java.io.IOException;
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 
 public class ScepsConfigTest {
 
     @Test
-    public void testReadXmlGlobalConfigFile() {
+    public void testReadGlobalConfigFile() {
 
-        final String globalConfigXmlPath = getClass().getResource("Global_Configuration.xml").getPath();
+        final String globalConfigXmlPath =
+                Objects.requireNonNull(getClass().getResource("Global_Configuration.xml")).getPath();
         Document document = null;
         try {
             document = ScepsConfig.readXMLDocumentFromFile(globalConfigXmlPath);
@@ -79,40 +73,191 @@ public class ScepsConfigTest {
     }
 
     @Test
-    public void testReadXmlLocalConfigFile() {
-//        <?xml version="1.0" encoding="UTF-8"?>
-//        <Forward_Model_Local_Configuration version="04.15.33">
-//           <parameter name="frequencies" description="" type="ARRAY" elementType="FLOAT" dims="5">1.410 6.925 10.650 18.700 36.500</parameter>
-//           <parameter name="bands" description="" type="STRING" dims="1">L C X KuKa</parameter>
-//           <parameter name="v_polarization" description="" type="ARRAY" elementType="INTEGER" dims="5">1 1 1 1 1</parameter>
-//           <parameter name="h_polarization" description="" type="ARRAY" elementType="INTEGER" dims="5">1 1 1 1 1</parameter>
-//           <parameter name="atmosphere_adding" description="" type="ARRAY" elementType="INTEGER" dims="5">1 1 1 1 1</parameter>
-//           <parameter name="land_only_coast" description="" type="ARRAY" elementType="INTEGER" dims="1">0</parameter>
-//           <parameter name="zenith_angle" description="freqsxscans" type="FLOAT" dims="1 5" >40 55 55 55 55</parameter>
-//           <parameter name="azimuth_angle" description="freqsxscans" type="FLOAT" dims="1 5" >0 0 0 0 0  </parameter>
-//           <parameter name="sensor_height" description="" type="FLOAT">853</parameter>
-//           <parameter name="data_thinning" description="" type="INTEGER">-2</parameter>
-//           <parameter name="emis_ice_smoothing" description="" type="FLOAT">1</parameter>
-//           <parameter name="emis_land_external" type="FLOAT" dims="1 1" >-999 </parameter>
-//        </Forward_Model_Local_Configuration>
+    public void testReadGeoInputsLocalConfigFile() {
 
-        final String fwModelTestString =
-                "<Forward_Model_Local_Configuration version=\"04.15.33\">\n" +
-                        "<parameter name=\"frequencies\" description=\"\" type=\"ARRAY\" elementType=\"FLOAT\" dims=\"5\">1.410 6.925 10.650 18.700 36.500</parameter>\n" +
-                        "<parameter name=\"bands\" description=\"\" type=\"STRING\" dims=\"1\">L C X KuKa</parameter>\n" +
-                        "<parameter name=\"v_polarization\" description=\"\" type=\"ARRAY\" elementType=\"INTEGER\" dims=\"5\">1 1 1 1 1</parameter>\n" +
-                        "<parameter name=\"h_polarization\" description=\"\" type=\"ARRAY\" elementType=\"INTEGER\" dims=\"5\">1 1 1 1 1</parameter>\n" +
-                        "<parameter name=\"atmosphere_adding\" description=\"\" type=\"ARRAY\" elementType=\"INTEGER\" dims=\"5\">1 1 1 1 1</parameter>\n" +
-                        "<parameter name=\"land_only_coast\" description=\"\" type=\"ARRAY\" elementType=\"INTEGER\" dims=\"1\">0</parameter>\n" +
-                        "<parameter name=\"zenith_angle\" description=\"freqsxscans\" type=\"FLOAT\" dims=\"1 5\" >40 55 55 55 55</parameter>\n" +
-                        "<parameter name=\"azimuth_angle\" description=\"freqsxscans\" type=\"FLOAT\" dims=\"1 5\" >0 0 0 0 0  </parameter>\n" +
-                        "<parameter name=\"sensor_height\" description=\"\" type=\"FLOAT\">853</parameter>\n" +
-                        "<parameter name=\"data_thinning\" description=\"\" type=\"INTEGER\">-2</parameter>\n" +
-                        "<parameter name=\"emis_ice_smoothing\" description=\"\" type=\"FLOAT\">1</parameter>\n" +
-                        "<parameter name=\"emis_land_external\" type=\"FLOAT\" dims=\"1 1\" >-999 </parameter>\n" +
-                        "</Forward_Model_Local_Configuration>";
+        final String globalConfigXmlPath =
+                Objects.requireNonNull(getClass().
+                        getResource("GeoInputs_Extract_Local_Configuration_geocard2_day1.xml")).getPath();
+        Document document = null;
+        try {
+            document = ScepsConfig.readXMLDocumentFromFile(globalConfigXmlPath);
+        } catch (Exception e) {
+            fail("Cannot parse '" + globalConfigXmlPath + "'");
+        }
 
-        // todo
-        assertTrue(true);
+        //Verify XML Content
+
+        Element root = document.getDocumentElement();
+        assertNotNull(root);
+        assertEquals("GeoInputs_Extract_Local_Configuration", root.getNodeName());
+        assertNotNull(root.getAttributes());
+        assertEquals(1, root.getAttributes().getLength());
+        assertEquals("0.1", root.getAttribute("version"));
+
+        NodeList nList = document.getElementsByTagName("parameter");
+        assertNotNull(nList);
+        assertEquals(6, nList.getLength());
+
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node node = nList.item(i);
+            assertNotNull(node);
+            assertEquals("parameter", node.getNodeName());
+            assertEquals(Node.ELEMENT_NODE, node.getNodeType());
+        }
+
+        Element elem = (Element) nList.item(0);
+        assertEquals(3, elem.getAttributes().getLength());
+        assertEquals("", elem.getAttribute("description"));
+        assertEquals("scene_type", elem.getAttribute("name"));
+        assertEquals("STRING", elem.getAttribute("type"));
+        assertNotNull(elem.getFirstChild());
+        assertNotNull(elem.getFirstChild().getTextContent());
+        assertEquals("geo-nemo", elem.getFirstChild().getTextContent());
+
+        elem = (Element) nList.item(2);
+        assertEquals(3, elem.getAttributes().getLength());
+        assertEquals("", elem.getAttribute("description"));
+        assertEquals("data_folder", elem.getAttribute("name"));
+        assertEquals("STRING", elem.getAttribute("type"));
+        assertNotNull(elem.getFirstChild());
+        assertNotNull(elem.getFirstChild().getTextContent());
+        assertEquals("E2E_HOME/InputData/GeoInputData/GeoCardScenes/SCEPS_ts2", elem.getFirstChild().getTextContent());
+
+        elem = (Element) nList.item(4);
+        assertEquals(5, elem.getAttributes().getLength());
+        assertEquals("", elem.getAttribute("description"));
+        assertEquals("latitude_filter", elem.getAttribute("name"));
+        assertEquals("ARRAY", elem.getAttribute("type"));
+        assertEquals("FLOAT", elem.getAttribute("elementType"));
+        assertEquals("2", elem.getAttribute("dims"));
+        assertEquals(1, elem.getChildNodes().getLength());
+        assertEquals("-90 90", elem.getChildNodes().item(0).getTextContent());
+        final String[] latitudes = elem.getChildNodes().item(0).getTextContent().split(" ");
+        assertEquals(2, latitudes.length);
+        assertEquals(-90, Integer.parseInt(latitudes[0]));
+        assertEquals(90, Integer.parseInt(latitudes[1]));
+    }
+
+    @Test
+    public void testReadForwardModelLocalConfigFile() {
+
+        final String globalConfigXmlPath =
+                Objects.requireNonNull(getClass().getResource("Forward_Model_Local_Configuration_geocard2_day1.xml")).getPath();
+        Document document = null;
+        try {
+            document = ScepsConfig.readXMLDocumentFromFile(globalConfigXmlPath);
+        } catch (Exception e) {
+            fail("Cannot parse '" + globalConfigXmlPath + "'");
+        }
+
+        //Verify XML Content
+
+        Element root = document.getDocumentElement();
+        assertNotNull(root);
+        assertEquals("Forward_Model_Local_Configuration", root.getNodeName());
+        assertNotNull(root.getAttributes());
+        assertEquals(1, root.getAttributes().getLength());
+        assertEquals("04.15.33", root.getAttribute("version"));
+
+        NodeList nList = document.getElementsByTagName("parameter");
+        assertNotNull(nList);
+        assertEquals(12, nList.getLength());
+
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node node = nList.item(i);
+            assertNotNull(node);
+            assertEquals("parameter", node.getNodeName());
+            assertEquals(Node.ELEMENT_NODE, node.getNodeType());
+        }
+
+        Element elem = (Element) nList.item(0);
+        assertEquals(5, elem.getAttributes().getLength());
+        assertEquals("", elem.getAttribute("description"));
+        assertEquals("frequencies", elem.getAttribute("name"));
+        assertEquals("ARRAY", elem.getAttribute("type"));
+        assertEquals("FLOAT", elem.getAttribute("elementType"));
+        assertEquals("5", elem.getAttribute("dims"));
+        assertNotNull(elem.getChildNodes());
+        assertNotNull(elem.getChildNodes().item(0).getTextContent());
+        assertEquals("1.410 6.925 10.650 18.700 36.500", elem.getChildNodes().item(0).getTextContent());
+        final String[] frequencies = elem.getChildNodes().item(0).getTextContent().split(" ");
+        assertEquals(5, frequencies.length);
+        assertEquals(1.410, Double.parseDouble(frequencies[0]), Double.MIN_VALUE);
+        assertEquals(6.925, Double.parseDouble(frequencies[1]), Double.MIN_VALUE);
+        assertEquals(10.650, Double.parseDouble(frequencies[2]), Double.MIN_VALUE);
+        assertEquals(18.700, Double.parseDouble(frequencies[3]), Double.MIN_VALUE);
+        assertEquals(36.500, Double.parseDouble(frequencies[4]), Double.MIN_VALUE);
+
+        elem = (Element) nList.item(1);
+        assertEquals(4, elem.getAttributes().getLength());
+        assertEquals("", elem.getAttribute("description"));
+        assertEquals("bands", elem.getAttribute("name"));
+        assertEquals("STRING", elem.getAttribute("type"));
+        assertEquals("1", elem.getAttribute("dims"));
+        assertNotNull(elem.getChildNodes());
+        assertEquals("L C X KuKa", elem.getChildNodes().item(0).getTextContent());
+        final String[] bands = elem.getChildNodes().item(0).getTextContent().split(" ");
+        assertEquals(4, bands.length);
+        assertEquals("L", bands[0]);
+        assertEquals("C", bands[1]);
+        assertEquals("X", bands[2]);
+        assertEquals("KuKa", bands[3]);
+
+        elem = (Element) nList.item(2);
+        assertEquals(5, elem.getAttributes().getLength());
+        assertEquals("", elem.getAttribute("description"));
+        assertEquals("v_polarization", elem.getAttribute("name"));
+        assertEquals("ARRAY", elem.getAttribute("type"));
+        assertEquals("INTEGER", elem.getAttribute("elementType"));
+        assertEquals("5", elem.getAttribute("dims"));
+        assertNotNull(elem.getChildNodes());
+        assertEquals("1 1 1 1 1", elem.getChildNodes().item(0).getTextContent());
+        final String[] vpol = elem.getChildNodes().item(0).getTextContent().split(" ");
+        assertEquals(5, vpol.length);
+        assertEquals(1, Integer.parseInt(vpol[0]));
+        assertEquals(1, Integer.parseInt(vpol[1]));
+        assertEquals(1, Integer.parseInt(vpol[2]));
+        assertEquals(1, Integer.parseInt(vpol[3]));
+        assertEquals(1, Integer.parseInt(vpol[4]));
+
+        elem = (Element) nList.item(5);
+        assertEquals(5, elem.getAttributes().getLength());
+        assertEquals("", elem.getAttribute("description"));
+        assertEquals("land_only_coast", elem.getAttribute("name"));
+        assertEquals("ARRAY", elem.getAttribute("type"));
+        assertEquals("INTEGER", elem.getAttribute("elementType"));
+        assertEquals("1", elem.getAttribute("dims"));
+        assertNotNull(elem.getChildNodes());
+        final String loc = elem.getChildNodes().item(0).getTextContent();
+        assertEquals(0, Integer.parseInt(loc));
+
+        elem = (Element) nList.item(6);
+        assertEquals(4, elem.getAttributes().getLength());
+        assertEquals("freqsxscans", elem.getAttribute("description"));
+        assertEquals("zenith_angle", elem.getAttribute("name"));
+        assertEquals("FLOAT", elem.getAttribute("type"));
+        assertEquals("1 5", elem.getAttribute("dims"));
+        assertNotNull(elem.getChildNodes());
+        assertNotNull(elem.getChildNodes().item(0).getTextContent());
+        assertEquals("40 55 55 55 55", elem.getChildNodes().item(0).getTextContent());
+        final String[] za = elem.getChildNodes().item(0).getTextContent().split(" ");
+        assertEquals(5, frequencies.length);
+        assertEquals(40, Double.parseDouble(za[0]), Double.MIN_VALUE);
+        assertEquals(55, Double.parseDouble(za[1]), Double.MIN_VALUE);
+        assertEquals(55, Double.parseDouble(za[2]), Double.MIN_VALUE);
+        assertEquals(55, Double.parseDouble(za[3]), Double.MIN_VALUE);
+        assertEquals(55, Double.parseDouble(za[4]), Double.MIN_VALUE);
+
+        elem = (Element) nList.item(11);
+        assertEquals(3, elem.getAttributes().getLength());
+        assertEquals("emis_land_external", elem.getAttribute("name"));
+        assertEquals("FLOAT", elem.getAttribute("type"));
+        assertEquals("1 1", elem.getAttribute("dims"));
+        assertNotNull(elem.getChildNodes());
+        assertNotNull(elem.getChildNodes().item(0).getTextContent());
+        final String ele = elem.getChildNodes().item(0).getTextContent();
+        assertEquals("-999 ", ele);
+        assertEquals(-999, Double.parseDouble(ele), Double.MIN_VALUE);
+
     }
 }
